b5c9396a74f49ca2361520979f067d58317e80a8
Shifakhan1929 <155301994+Shifakhan1929@users.noreply.github.com>
2025-05-23 16:06:08 +0530

    create new color and theme file

---
+++ lib/utils/utils2/colors2.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFF8ECAE6);
+  static const Color blue = Color(0xFF219EBC);
+  static const Color darkBlue = Color(0xFF023047);
+  static const Color amber = Color(0xFFFFB703);
+}
---
+++ lib/utils/utils2/theme2.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'colors2.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: AppColors.blue,
+    scaffoldBackgroundColor: AppColors.lightBlue,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.blue,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: AppColors.amber,
+    ),
+    colorScheme: ColorScheme.light(
+      primary: AppColors.blue,
+      secondary: AppColors.amber,
+      background: AppColors.lightBlue,
+      surface: Colors.white,
+      onPrimary: Colors.white,
+      onSecondary: Colors.black,
+      onBackground: AppColors.darkBlue,
+      onSurface: AppColors.darkBlue,
+    ),
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        color: AppColors.darkBlue,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.darkBlue,
+      ),
+    ),
+  );
+}
